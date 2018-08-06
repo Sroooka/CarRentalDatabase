946ae3196b9f5fd4105ec65eba5392917bc045e1
Sroka <tomasz.sroka@capgemini.com>
2018-08-06 12:02:35 +0200

    Customer Table Create

--- DDL.sql
+++ DDL.sql
@@ -12,11 +12,11 @@ CREATE TABLE IF NOT EXISTS CarRental.EMPLOYEE (
   ID INT NOT NULL,
   NAME VARCHAR(20) NULL,
   SURNAME VARCHAR(20) NULL,
-  POSITION ENUM('dealer', 'manager', 'accountant'),
-  PESEL INTEGER(11),
-  PHONE VARCHAR(20),
-  EMAIL VARCHAR(30),
-  LOCATION_ID INT,
+  POSITION ENUM('dealer', 'manager', 'accountant') NULL,
+  PESEL INTEGER(11) NULL,
+  PHONE VARCHAR(20) NULL,
+  EMAIL VARCHAR(30) NULL,
+  LOCATION_ID INT NULL,
   PRIMARY KEY (ID))
 ENGINE = InnoDB;
 
@@ -35,14 +35,33 @@ CREATE TABLE IF NOT EXISTS CarRental.CAR (
             "cabriolet", 
             "coupe",
             "pickup",
-            "4wd"),
-  COLOR VARCHAR(12),
-  ENGINE_SIZE INT(5),
-  POWER INT(4),
-  MILEAGE INT(7),
-  SEATS INT(2),
-  FUEL ENUM("petrol", "diesel", "lpg", "electric"),
-  CURRENT_LOCATION_ID INT,
+            "4wd") NULL,
+  COLOR VARCHAR(12) NULL,
+  ENGINE_SIZE INT(5) NULL,
+  POWER INT(4) NULL,
+  MILEAGE INT(7) NULL,
+  SEATS INT(2) NULL,
+  FUEL ENUM("petrol", "diesel", "lpg", "electric") NULL,
+  CURRENT_LOCATION_ID INT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `CarRental`.`CUSTOMER`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS CarRental.CUSTOMER (
+  ID INT NOT NULL,
+  NAME VARCHAR(20) NULL,
+  SURNAME VARCHAR(20) NULL,
+  ADDRESS VARCHAR(30) NULL,
+  CITY VARCHAR(30) NULL,
+  POSTAL_CODE INT(5) NULL,
+  BIRTH_DATE DATE NULL,
+  PHONE VARCHAR(20) NULL,
+  EMAIL VARCHAR(30) NULL,
+  CREDIT_CARD_NUMBER INT(19) NULL,
+  IDENTITY_CARD_NUMBER VARCHAR(15),
+  DRIVING_LICENSE_NUMBER VARCHAR(15),
   PRIMARY KEY (ID))
 ENGINE = InnoDB;
 
