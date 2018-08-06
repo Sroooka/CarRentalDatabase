df9c1d6adb013f6dfe3b87e7cef1ceb2d0b46bf9
Sroka <tomasz.sroka@capgemini.com>
2018-08-06 13:06:41 +0200

    Init employee positon and car types

---
+++ DML.sql
@@ -0,0 +1,15 @@
+-- insert Car Types
+INSERT INTO car_type (id, type) VALUES (1, 'sedan');
+INSERT INTO car_type (id, type) VALUES (2, 'wagon');
+INSERT INTO car_type (id, type) VALUES (3, 'suv');
+INSERT INTO car_type (id, type) VALUES (4, 'hatchback');
+INSERT INTO car_type (id, type) VALUES (5, 'cabriolet');
+INSERT INTO car_type (id, type) VALUES (6, 'coupe');
+INSERT INTO car_type (id, type) VALUES (7, 'pickup');
+INSERT INTO car_type (id, type) VALUES (8, '4wd');
+
+-- insert Employee Positions
+INSERT INTO position (id, position) VALUES (1, 'dealer');
+INSERT INTO position (id, position) VALUES (2, 'manager');
+INSERT INTO position (id, position) VALUES (3, 'accountant');
+
