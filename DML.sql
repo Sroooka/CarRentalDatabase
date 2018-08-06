1d2ceee79f612b4bbc5fb38ca21633943893ae6e
Sroka <tomasz.sroka@capgemini.com>
2018-08-06 13:42:15 +0200

    Insert locations

--- DML.sql
+++ DML.sql
@@ -14,3 +14,13 @@ INSERT INTO position (id, position) VALUES (2, 'manager');
 INSERT INTO position (id, position) VALUES (3, 'accountant');
 
 -- insert Location
+INSERT 	INTO location (id, address, city, postal_code, phone, email) 
+		VALUES (1, 'Bukowska 150', 'Poznan', '60123', '+(48)61-255-25-25', 'office.poznan@carrentalsroka.com');
+INSERT 	INTO location (id, address, city, postal_code, phone, email) 
+		VALUES (2, 'Niedzialkowskiego 1', 'Warszawa', '04112', '+(48)22-525-25-25', 'office.warszawa@carrentalsroka.com');
+INSERT 	INTO location (id, address, city, postal_code, phone, email) 
+		VALUES (3, 'Naramowiska 500', 'Gdansk', '12542', '+(48)14-143-25-25', 'office.gdansk@carrentalsroka.com');
+INSERT 	INTO location (id, address, city, postal_code, phone, email) 
+		VALUES (4, 'Przemyslowa 9', 'Krakow', '60123', '+(48)76-655-25-25', 'office.krakow@carrentalsroka.com');
+INSERT 	INTO location (id, address, city, postal_code, phone, email) 
+		VALUES (5, 'Dabrowskiego 13/9', 'Lodz', '87665', '+(48)17-654-25-25', 'office.lodz@carrentalsroka.com');
