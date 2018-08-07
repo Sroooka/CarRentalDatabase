0d7125976835e08cc7431d3c6f0e6bece0e70c1e
Sroka <tomasz.sroka@capgemini.com>
2018-08-07 09:59:46 +0200

    Relations updated

--- DDL.sql
+++ DDL.sql
@@ -2,20 +2,47 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+-- -----------------------------------------------------
+-- Clear tables before creating
+-- -----------------------------------------------------
+DROP DATABASE IF EXISTS `CarRentalSroka`;
+
+-- -----------------------------------------------------
+-- Create schema
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `CarRentalSroka` DEFAULT CHARACTER SET utf8 ;
 USE `CarRentalSroka`;
 
 -- -----------------------------------------------------
--- Clear tables before creating
+-- Table `CarRentalSroka`.`POSITION`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS 	
-			CarRentalSroka.CAR, 
-			CarRentalSroka.CAR_CARER, 
-            CarRentalSroka.CAR_TYPE, 
-            CarRentalSroka.CUSTOMER, 
-            CarRentalSroka.EMPLOYEE, 
-            CarRentalSroka.LOCATION, 
-            CarRentalSroka.POSITION;
+CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`POSITION` (
+  ID INT NOT NULL,
+  POSITION VARCHAR(30),
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `CarRentalSroka`.`CAR_TYPE`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`CAR_TYPE` (
+  ID INT NOT NULL,
+  TYPE VARCHAR(30),
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `CarRentalSroka`.`LOCATION`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`LOCATION` (
+  ID INT NOT NULL,
+  ADDRESS VARCHAR(30) DEFAULT '',
+  CITY VARCHAR(30) DEFAULT '',
+  POSTAL_CODE VARCHAR(6) DEFAULT '00-000',
+  PHONE VARCHAR(20) DEFAULT '+(48)000-000-000',
+  EMAIL VARCHAR(50) DEFAULT 'user@domain.com',
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `CarRentalSroka`.`EMPLOYEE`
@@ -28,10 +55,16 @@ CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`EMPLOYEE` (
   PESEL VARCHAR(11) DEFAULT '00000000000',
   LOCATION_ID INT DEFAULT 1,
   PRIMARY KEY (ID),
-  INDEX `employee_location_key` (`LOCATION_ID` ASC),
+  INDEX `employee_location_key_idx` (`LOCATION_ID` ASC),
+  INDEX `employee_position_key_idx` (`POSITION_ID` ASC),
   CONSTRAINT `employee_location_key`
     FOREIGN KEY (`LOCATION_ID`)
     REFERENCES `CarRentalSroka`.`Location` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `employee_position_key`
+    FOREIGN KEY (`POSITION_ID`)
+    REFERENCES `CarRentalSroka`.`POSITION` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -51,10 +84,16 @@ CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`CAR` (
   MILEAGE INT(7) DEFAULT 0,
   CURRENT_LOCATION_ID INT DEFAULT 1,
   PRIMARY KEY (ID),
-  INDEX `current_location_key` (`CURRENT_LOCATION_ID` ASC),
+  INDEX `current_location_key_idx` (`CURRENT_LOCATION_ID` ASC),
+  INDEX `type_key_idx` (`TYPE_ID` ASC),
   CONSTRAINT `current_location_key`
     FOREIGN KEY (`CURRENT_LOCATION_ID`)
     REFERENCES `CarRentalSroka`.`Location` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `car_type_key`
+    FOREIGN KEY (`TYPE_ID`)
+    REFERENCES `CarRentalSroka`.`CAR_TYPE` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -81,7 +120,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`RENTAL` (
   ID INT NOT NULL,
-  CUSTOMER_ID INT NULL,
+  CUSTOMER_ID INT,
   CAR_ID INT,
   RENT_BEGIN DATE DEFAULT '2000-1-1',
   RENT_END DATE DEFAULT '2000-1-1',
@@ -89,16 +128,16 @@ CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`RENTAL` (
   END_LOCATION_ID INT DEFAULT 1,
   COST INT DEFAULT 0,
   PRIMARY KEY (ID),
-  INDEX `customer_key` (`CUSTOMER_ID` ASC),
-  INDEX `car_key` (`CAR_ID` ASC),
-  INDEX `start_location_key` (`START_LOCATION_ID` ASC),
-  INDEX `end_location_key` (`END_LOCATION_ID` ASC),
-  CONSTRAINT `customer_key`
+  INDEX `rental_customer_key_idx` (`CUSTOMER_ID` ASC),
+  INDEX `rental_car_key_idx` (`CAR_ID` ASC),
+  INDEX `start_location_key_idx` (`START_LOCATION_ID` ASC),
+  INDEX `end_location_key_idx` (`END_LOCATION_ID` ASC),
+  CONSTRAINT `rental_customer_key`
     FOREIGN KEY (`CUSTOMER_ID`)
     REFERENCES `CarRentalSroka`.`CUSTOMER` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `car_key`
+  CONSTRAINT `rental_car_key`
     FOREIGN KEY (`CAR_ID`)
     REFERENCES `CarRentalSroka`.`CAR` (`ID`)
     ON DELETE NO ACTION
@@ -116,27 +155,14 @@ CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`RENTAL` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `CarRentalSroka`.`LOCATION`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`LOCATION` (
-  ID INT NOT NULL,
-  ADDRESS VARCHAR(30) DEFAULT '',
-  CITY VARCHAR(30) DEFAULT '',
-  POSTAL_CODE VARCHAR(6) DEFAULT '00-000',
-  PHONE VARCHAR(20) DEFAULT '+(48)000-000-000',
-  EMAIL VARCHAR(50) DEFAULT 'user@domain.com',
-  PRIMARY KEY (ID))
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
 -- Table `CarRentalSroka`.`CAR_CARER`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`CAR_CARER` (
   EMPLOYEE_ID INT NOT NULL,
   CAR_ID INT NOT NULL,
-  INDEX `car_key_map` (`CAR_ID` ASC),
-  INDEX `employee_key_map` (`EMPLOYEE_ID` ASC),
-  CONSTRAINT car_key_map
+  INDEX `car_key_map_idx` (`CAR_ID` ASC),
+  INDEX `employee_key_map_idx` (`EMPLOYEE_ID` ASC),
+  CONSTRAINT `car_key_map`
     FOREIGN KEY (`CAR_ID`)
     REFERENCES `CarRentalSroka`.`CAR` (`ID`)
     ON DELETE NO ACTION
@@ -146,24 +172,6 @@ CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`CAR_CARER` (
     REFERENCES `CarRentalSroka`.`EMPLOYEE` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `CarRentalSroka`.`CAR_TYPE`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`CAR_TYPE` (
-  ID INT NOT NULL,
-  TYPE VARCHAR(30),
-  PRIMARY KEY (ID))
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `CarRentalSroka`.`POSITION`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `CarRentalSroka`.`POSITION` (
-  ID INT NOT NULL,
-  POSITION VARCHAR(30),
-  PRIMARY KEY (ID))
 ENGINE = InnoDB;
 
 SET SQL_MODE=@OLD_SQL_MODE;
--- DML.sql
+++ DML.sql
@@ -1,3 +1,8 @@
+-- insert Employee Positions
+INSERT INTO position (id, position) VALUES (1, 'dealer');
+INSERT INTO position (id, position) VALUES (2, 'manager');
+INSERT INTO position (id, position) VALUES (3, 'accountant');
+
 -- insert Car Types
 INSERT INTO car_type (id, type) VALUES (1, 'sedan');
 INSERT INTO car_type (id, type) VALUES (2, 'wagon');
@@ -7,11 +12,6 @@ INSERT INTO car_type (id, type) VALUES (5, 'cabriolet');
 INSERT INTO car_type (id, type) VALUES (6, 'coupe');
 INSERT INTO car_type (id, type) VALUES (7, 'pickup');
 INSERT INTO car_type (id, type) VALUES (8, '4wd');
-
--- insert Employee Positions
-INSERT INTO position (id, position) VALUES (1, 'dealer');
-INSERT INTO position (id, position) VALUES (2, 'manager');
-INSERT INTO position (id, position) VALUES (3, 'accountant');
 
 -- insert Location
 INSERT 	INTO location (id, address, city, postal_code, phone, email) 
@@ -27,54 +27,53 @@ INSERT 	INTO location (id, address, city, postal_code, phone, email)
         
 -- insert Employee
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(1, 'Antoni', 'FFFFUUUUUUUUU', 2, '91091213113', 1);
+		VALUES(1, 'Antoni', 'kFowadis', 2, '99010254841', 1);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(2, 'Jakub', 'Kowalski', 3, '91091213113', 1);
+		VALUES(2, 'Jakub', 'Kowalski', 3, '98050687458', 1);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(3, 'Jan', 'Wisniewski', 1, '91091213113', 1);
+		VALUES(3, 'Jan', 'Wisniewski', 1, '97045245215', 1);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(4, 'Szymon', 'Wojcik', 1, '91091213113', 1);
+		VALUES(4, 'Szymon', 'Wojcik', 1, '96145215985', 1);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(5, 'Franciszek', 'Kowalczyk', 1, '91091213113', 1);
+		VALUES(5, 'Franciszek', 'Kowalczyk', 1, '95632569852', 1);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(6, 'Filip', 'Kaminski', 2, '91091213113', 2);
+		VALUES(6, 'Filip', 'Kaminski', 2, '94154245875', 2);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(7, 'Aleksander', 'Lewandowski', 3, '91091213113', 2);
+		VALUES(7, 'Aleksander', 'Lewandowski', 3, '93458745896', 2);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(8, 'Wojciech', 'Zielinski', 1, '91091213113', 2);
+		VALUES(8, 'Wojciech', 'Zielinski', 1, '92145854854', 2);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(9, 'Kacper', 'Szymanski', 1, '91091213113', 2);
+		VALUES(9, 'Kacper', 'Szymanski', 1, '90121454124', 2);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(10, 'Mikolaj', 'Wozniak', 1, '91091213113', 2);
+		VALUES(10, 'Mikolaj', 'Wozniak', 1, '89521458585', 2);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(11, 'Adam', 'Dabrowski', 2, '91091213113', 3);
+		VALUES(11, 'Adam', 'Dabrowski', 2, '88454575865', 3);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(12, 'Michau', 'Kozlowski', 3, '91091213113', 3);
+		VALUES(12, 'Michau', 'Kozlowski', 3, '87145252523', 3);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(13, 'Marcel', 'Jankowski', 1, '91091213113', 3);
+		VALUES(13, 'Marcel', 'Jankowski', 1, '86147852545', 3);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(14, 'Wiktor', 'Mazur', 1, '91091213113', 3);
+		VALUES(14, 'Wiktor', 'Mazur', 1, '85124587595', 3);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(15, 'Piotr', 'Wojciechowski', 1, '91091213113', 3);
+		VALUES(15, 'Piotr', 'Wojciechowski', 1, '84754854854', 3);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(16, 'Stanislaw', 'Kwiatkowski', 2, '91091213113', 4);
+		VALUES(16, 'Stanislaw', 'Kwiatkowski', 2, '83111145211', 4);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(17, 'Igor', 'Krawczyk', 3, '91091213113', 4);
+		VALUES(17, 'Igor', 'Krawczyk', 3, '82145748587', 4);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(18, 'Mateusz', 'Kaczmarek', 1, '91091213113', 4);
+		VALUES(18, 'Mateusz', 'Kaczmarek', 1, '81458585858', 4);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(19, 'Bartosz', 'Piotrowski', 1, '91091213113', 4);
+		VALUES(19, 'Bartosz', 'Piotrowski', 1, '80121252421', 4);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(20, 'Nikodem', 'Grabowski', 1, '91091213113', 4);
+		VALUES(20, 'Nikodem', 'Grabowski', 1, '79369636965', 4);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(21, 'Leon', 'Zajac', 2, '91091213113', 5);
+		VALUES(21, 'Leon', 'Zajac', 2, '78414141414', 5);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(22, 'Milosz', 'Pawlowski', 3, '91091213113', 5);
+		VALUES(22, 'Milosz', 'Pawlowski', 3, '77142515895', 5);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(23, 'Maksymilian', 'Michalski', 1, '91091213113', 5);
+		VALUES(23, 'Maksymilian', 'Michalski', 1, '76145854584', 5);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(24, 'Tymon', 'Krol', 1, '91091213113', 5);
+		VALUES(24, 'Tymon', 'Krol', 1, '75142525252', 5);
 INSERT INTO employee (id, name, surname, position_id, pesel, location_id)
-		VALUES(25, 'Alan', 'Wieczorek', 1, '91091213113', 5);
-
+		VALUES(25, 'Alan', 'Wieczorek', 1, '74256256940', 5);
 
